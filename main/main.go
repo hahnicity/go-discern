6a42c8261bebbc1c805989a3b12b9b4ac1d4db98
hahnicity <grehm87@gmail.com>
2013-10-05 21:15:23 -0700

    Adding argument parser Adding an argument parser to main.go

--- main/main.go
+++ main/main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+    "flag"
     "github.com/hahnicity/go-discern"
     "github.com/hahnicity/go-discern/data"
 )
 
-//XXX Make argument parser
+var (
+    processes int
+    workers   int
+    year      string
+)
+
+func parseArgs() {
+    flag.IntVar(
+        &processes,
+        "p",
+        20,
+        "The number of parallel processes we want operating",
+    )
+    flag.IntVar(
+        &workers,
+        "workers",
+        50,
+        "The number of workers we want to have in our pool",
+    )
+    flag.StringVar(
+        &year,
+        "year",
+        "2013",
+        "The year we wish to look for stats in",
+    )
+    flag.Parse()
+}
 
 func main() {
+    parseArgs()
     work := make(chan discern.WikiRequest)
-    b := discern.MakeBalancer(600)
-    go b.Balance(work)
-    discern.Requester("2012", data.SP500, 0, work)
+    go discern.MakeBalancer(workers).Balance(work)
+    discern.Requester(year, data.SP500, processes, work)
 }
