23ac7079ec2eccb9672e73fa0937b0022dd5faef
hahnicity <grehm87@gmail.com>
2013-10-14 22:54:34 -0700

    Adding booleans to determine if we should analyze means/tweets

--- main/main.go
+++ main/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 var (
+    analyzeMeans   bool
+    analyzeTweets  bool
     closeRequests  bool
     maxRequests    int
     meanPercentile float64
@@ -41,6 +43,18 @@ func parseArgs() {
         .99,
         "The page view percentile we wish to look for. Must be less than 1",
     )
+    flag.BoolVar(
+        &closeRequests,
+        "closeRequests",
+        false,
+        "Set to true if you want to close wiki requests after they have been made",
+    )
+    flag.BoolVar(
+        &analyzeMeans,
+        "analyzeMeans",
+        false,
+        "Analyze which companies have the highest means of all the companies surveyed",
+    )
     flag.Float64Var(
         &meanPercentile,
         "meanp",
@@ -48,10 +62,10 @@ func parseArgs() {
         "The mean page view percentile we wish to look for",
     )
     flag.BoolVar(
-        &closeRequests,
-        "closeRequests",
-        false,
-        "Set to true if you want to close wiki requests after they have been made",
+        &analyzeTweets,
+        "analyzeTweets",
+        true,
+        "Analyze tweets of companies with recent news activity",
     )
     flag.Parse()
     if viewPercentile >= 1.0 || meanPercentile >= 1.0 {
@@ -61,7 +75,15 @@ func parseArgs() {
 
 func main() {
     parseArgs()
-    r := discern.NewRequester(closeRequests, maxRequests, meanPercentile, viewPercentile, year)
+    r := discern.NewRequester(
+        analyzeMeans, 
+        analyzeTweets, 
+        closeRequests, 
+        maxRequests, 
+        meanPercentile, 
+        viewPercentile, 
+        year,
+    )
     go discern.MakeBalancer(workers).Balance(r.Work)
     r.MakeRequests(data.SP500)
 }
--- requester.go
+++ requester.go
@@ -6,19 +6,49 @@ import (
 )
 
 type Requester struct {
+    // The number of active requests that are being run with wikipedia
     activeRequests int
+
+    // All response received from wikipedia
     allResponses   []*WikiResponse
+
+    // Analyze the mean views of all companies on wikipedia
+    analyzeMeans   bool
+
+    // Analyze tweets regarding companies that are currently relevant on wikipedia
+    analyzeTweets  bool
+
+    // Close all wikipedia requests after they are made
     closeRequests  bool
+
+    // The maximum number of requests that can be run concurrently
     maxRequests    int
+
+    // The percentile at which comapnies with mean views above that should be shown
     meanPercentile float64
+
+    // Channel of active work
     Work           chan WikiRequest
+
+    // The percentile at which a date with number of views above that should be shown
     viewPercentile float64
+
+    // The year to analyze requests
     year           string
 }
 
-func NewRequester(closeRequests bool, maxRequests int, meanPercentile, viewPercentile float64, year string) (r *Requester){
+// Make a new Requester object. 
+func NewRequester(analyzeMeans bool,
+                  analyzeTweets bool,
+                  closeRequests bool, 
+                  maxRequests int, 
+                  meanPercentile float64, 
+                  viewPercentile float64, 
+                  year string) (r *Requester){
     r = new(Requester)    
     r.activeRequests = 0
+    r.analyzeMeans = analyzeMeans
+    r.analyzeTweets = analyzeTweets
     r.closeRequests = closeRequests
     r.maxRequests = maxRequests
     r.meanPercentile = meanPercentile
@@ -28,6 +58,9 @@ func NewRequester(closeRequests bool, maxRequests int, meanPercentile, viewPerce
     return
 }
 
+// Given a map of companies and their corresponding wikipedia pages, make
+// requests to stats.grok.se so that we can get statistics as to how frequently
+// people are viewing their pages
 func (r *Requester) MakeRequests(companies map[string]string) {
     c := make(chan *WikiResponse)
     for symbol, page := range companies {
@@ -43,8 +76,7 @@ func (r *Requester) MakeRequests(companies map[string]string) {
     r.Analyze()
 }
 
-// Throttle number of active requests
-// statsgrok.se is the problem here
+// Throttle number of active requests stats.grok.se is the problem here
 func (r *Requester) manageActiveProc(c chan *WikiResponse) {
     if r.activeRequests == r.maxRequests {
         resp := <- c
@@ -53,12 +85,13 @@ func (r *Requester) manageActiveProc(c chan *WikiResponse) {
     r.activeRequests--
 }
 
+// Analyze all responses received from wikipedia
 func (r *Requester) Analyze() {
-    r.analyzeMeans()
-    r.analyzePercentiles()
+    if r.analyzeMeans { r.means() }
+    r.percentiles()
 }
 
-func (r *Requester) analyzePercentiles() {
+func (r *Requester) percentiles() {
     for _, resp := range r.allResponses {
         dates := FindRecentDates(resp, r.viewPercentile)
         if len(dates) == 0 {
@@ -77,7 +110,7 @@ func (r *Requester) analyzePercentiles() {
     }
 }
 
-func (r *Requester) analyzeMeans() {
+func (r *Requester) means() {
     means := make(map[string]int)
     for _, resp := range r.allResponses {
         means[resp.Symbol] = FindMeanViews(resp)
